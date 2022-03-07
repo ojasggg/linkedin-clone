4be7d85947ecd67b798522e11eab1e7c1eb4792b
Ojash Gurung <OJASGGG@GMAIL.COM>
2022-03-07 22:57:32 +0545

    Post Flip Animation is Implemented with new react package name react-flip-move

--- src/Post.jsx
+++ src/Post.jsx
@@ -3,22 +3,22 @@ import ChatOutlinedIcon from "@mui/icons-material/ChatOutlined";
 import ShareOutlinedIcon from "@mui/icons-material/ShareOutlined";
 import SendOutlinedIcon from "@mui/icons-material/SendOutlined";
 import { Avatar } from "@mui/material";
-import React from "react";
+import React, { forwardRef } from "react";
 import InputOption from "./InputOption";
 import "./Post.css";
 
-function Post({ name, description, message, photoUrl }) {
+const Post = forwardRef(({ name, description, message, photoUrl }, ref) => {
   return (
-    <div className="post">
+    <div ref={ref} className="post">
       <div className="post__header">
-        <Avatar />
+        <Avatar src={photoUrl}>{name[0]}</Avatar>
         <div className="post__info">
           <h2>{name ? name : "no Name"}</h2>
           <p>{description}</p>
         </div>
       </div>
       <div className="post__body">
-        <h1>{message}</h1>
+        <p>{message}</p>
       </div>
       <div className="post__buttons">
         <InputOption Icon={ThumbUpAltOutlinedIcon} title="Like" color="gray" />
@@ -28,6 +28,6 @@ function Post({ name, description, message, photoUrl }) {
       </div>
     </div>
   );
-}
+});
 
 export default Post;
