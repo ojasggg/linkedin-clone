9c33a9545d05f3f44c1922d41744fe65d1d4bcf3
Ojash Gurung <OJASGGG@GMAIL.COM>
2022-03-07 20:03:03 +0545

    Implemented Provider inside our main app and gave store as redux store

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,27 @@
 import React from "react";
+import { Provider } from "react-redux";
 import Header from "./Header";
 import Sidebar from "./Sidebar";
 import Feed from "./Feed";
 import "./App.css";
 import Widget from "./Widget";
+import store from "./redux/store";
+import Login from "./Login";
 
 function App() {
   return (
-    <div className="app">
-      {/* Header  */}
-      <Header />
-      {/* Mid Body */}
-
-      <div className="app__body">
-        <Sidebar />
-        <Feed />
-        <Widget />
-
-        {/* Feed Section */}
-        {/* Widgets */}
+    <Provider store={store}>
+      <div className="app">
+        {/* Header  */}
+        <Header />
+        {/* Mid Body */}
+        <div className="app__body">
+          <Sidebar />
+          <Feed />
+          <Widget />
+        </div>
       </div>
-    </div>
+    </Provider>
   );
 }
 
